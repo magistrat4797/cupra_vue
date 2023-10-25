d87cb607c388f03a7876314637ef4542f94a4675
magistrat4797 <vano4797@gmail.com>
2023-10-25 18:53:19 +0200

    Some changes in components files structure - separated sections

--- src/views/LandingPage.vue
+++ src/views/LandingPage.vue
@@ -1,13 +1,13 @@
 <template>
   <div>
   <the-header />
-  <top-box>
+  <hero-section>
     <template #subtitle>CUPRA Formentor</template>
     <template #title>Wejdź do świata Cupra. Poznaj prawdziwą moc i&nbsp;sportowego ducha marki.</template>
     <template #actions>
       <base-link href="#" btn-style="secondary">Ruszaj na jazdę testową</base-link>
     </template>
-  </top-box>
+  </hero-section>
   <the-footer />
   </div>
 </template>
@@ -16,5 +16,5 @@
 import TheHeader from "@/components/TheHeader.vue";
 import TheFooter from '@/components/TheFooter.vue';
 import BaseLink from '@/components/base/BaseLink.vue';
-import TopBox from '@/components/TopBox.vue';
+import HeroSection from '@/components/sections/hero/HeroSection.vue';
 </script>
